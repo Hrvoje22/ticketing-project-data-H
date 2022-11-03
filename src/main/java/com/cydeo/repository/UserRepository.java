64fafe6d03af2c26ccb320cae2456e30031ca5e6
Hrvoje <hbrkljacic@gmail.com>
2022-11-03 17:42:04 -0400

    Part-1

---
+++ src/main/java/com/cydeo/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.cydeo.repository;
+
+import com.cydeo.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> { //goes with entities
+
+    User findByUserName(String username);
+
+
+
+}
