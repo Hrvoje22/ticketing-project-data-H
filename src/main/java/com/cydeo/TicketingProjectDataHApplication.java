17a3c785e57966bf14bb4d1d196d20913c9fb0c6
Hrvoje <hbrkljacic@gmail.com>
2022-11-03 19:10:25 -0400

    Part-1

--- src/main/java/com/cydeo/TicketingProjectDataHApplication.java
+++ src/main/java/com/cydeo/TicketingProjectDataHApplication.java
@@ -1,13 +1,22 @@
 package com.cydeo;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TicketingProjectDataHApplication {
 
     public static void main(String[] args) {
+
+
         SpringApplication.run(TicketingProjectDataHApplication.class, args);
+    }
+
+    @Bean
+    public ModelMapper mapper(){
+        return new ModelMapper();
     }
 
 }
