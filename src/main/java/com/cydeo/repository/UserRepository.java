cb6739c36b45e65b070f9d0e12ba7cb826140d75
Hrvoje <hbrkljacic@gmail.com>
2022-11-04 21:50:42 -0400

    Part-2

--- src/main/java/com/cydeo/repository/UserRepository.java
+++ src/main/java/com/cydeo/repository/UserRepository.java
@@ -3,9 +3,14 @@ package com.cydeo.repository;
 import com.cydeo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.transaction.Transactional;
+
 public interface UserRepository extends JpaRepository<User,Long> { //goes with entities
 
     User findByUserName(String username);
+
+    @Transactional //if something goes bad it goes back to original
+    void deleteByUserName(String username);
 
 
 
