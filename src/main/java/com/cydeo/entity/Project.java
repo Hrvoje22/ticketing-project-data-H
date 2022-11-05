53b4b4de4f39444988fec87994a320ad5537bcf3
Hrvoje <hbrkljacic@gmail.com>
2022-11-04 23:40:04 -0400

    Part-3

--- src/main/java/com/cydeo/entity/Project.java
+++ src/main/java/com/cydeo/entity/Project.java
@@ -1,12 +1,13 @@
 package com.cydeo.entity;
 
+import com.cydeo.enums.Status;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.Where;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.time.LocalDate;
 
 @Getter
 @Setter
@@ -15,6 +16,21 @@ import javax.persistence.Table;
 @Table(name = "projects")
 @Where(clause = "is_deleted=false")
 public class Project extends BaseEntity{
+
+    private String projectCode;
+    private String projectName;
+    @Column(columnDefinition = "DATE")
+    private LocalDate startDate;
+    @Column(columnDefinition = "DATE")
+    private LocalDate endDate;
+    @Enumerated(EnumType.STRING)
+    private Status projectStatus;
+    private String projectDetail;
+
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name = "manager_id")
+    private User assignedManager;
+
 
 
 
