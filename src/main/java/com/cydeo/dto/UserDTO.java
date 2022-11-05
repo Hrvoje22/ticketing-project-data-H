a4e4de755f7f4aeaf9443192e9961cd54190a18b
Hrvoje <hbrkljacic@gmail.com>
2022-11-05 14:47:27 -0400

    Part-3

--- src/main/java/com/cydeo/dto/UserDTO.java
+++ src/main/java/com/cydeo/dto/UserDTO.java
@@ -12,6 +12,16 @@ import javax.validation.constraints.*;
 @AllArgsConstructor
 public class UserDTO {
 
+    private Long id; //manager_id problem
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     @NotBlank
     @Size(max = 15, min = 2)
     private String firstName;
